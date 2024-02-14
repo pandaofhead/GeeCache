a4b9388760f0b35aef1824de957e79f6d51f0c64
Hongjin Quan <hq48@cornell.edu>
2024-02-14 14:59:13 -0500

    feat: add lru cache

---
+++ Notes.md
@@ -0,0 +1,12 @@
+# Caching
+
+## FIFO
+First In First Out: dosen't take frequency into account.
+
+## LFU
+Least Frequently Used: only take frequency into account.
+## LRU
+Least Recently Used: if some data has been used, then move it to the end of list, and the head of list will be the least recently used data, hence detele it.
+
+# LRU
+![LRU](/public/lru.jpg)
---
+++ lru/lru.go
@@ -0,0 +1,83 @@
+package lru
+
+import "container/list"
+
+// 1. Create a new cache with a map and a doubly linked list
+
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+// entry is used to hold a key/value pair in the cache
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value use Len to count how many bytes it takes
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// 2. lookup a key and move the corresponding element to the front of the list
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// 3. delete the least visited element
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry) // type assertion
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// 4. add a new key/value pair
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.nbytes > c.maxBytes {
+		c.RemoveOldest()
+	}
+}
+
+// 5. return the number of entries in the cache
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
